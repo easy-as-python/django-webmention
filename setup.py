9c037fe0bf0c47e0e8b373d4af9c33b2d094def4
Dane Hillard <github@danehillard.com>
2015-07-08 20:24:37 -0400

    Add model. Clean out urls.py. Update setup.py to read from requirements.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ import os
 from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-        README = readme.read()
+        readme = readme.read()
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as requirements_file:
+    requirements = [line.rstrip() for line in requirements_file if line != '\n']
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
@@ -13,6 +16,7 @@ setup(
     description='A pluggable implementation of webmention for Django projects.',
     author='Dane Hillard',
     author_email='github@danehillard.com',
-    long_description=README,
+    long_description=readme,
+    install_requires=requirements,
     url='https://github.com/daneah/django-webmention',
 )
