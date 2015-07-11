465fc5b24b16c00a27a2a6bb2d98ed38a8cd1bda
Dane Hillard <github@danehillard.com>
2015-07-10 23:34:56 -0400

    Update README. Add decorator from middleware

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A pluggable implementation of webmention for Django projects.
 
 `$ pip install django-webmention`
 
-Add `webmention` to `INSTALLED_APPS` in your project's `settings.py`
-
-Add `(r'^webmention', include('webmention.urls'), namespace='webmention')` to your project's top-level `urls.py`
+* Add `webmention` to `INSTALLED_APPS`
+* Add `(r'^webmention', include('webmention.urls'), namespace='webmention')` to top-level `urls.py`
+* Include webmention information by either:
+    * Adding `webmention.middleware.WebMentionMiddleware` to `MIDDLEWARE_CLASSES` (affects all views)
+    * Decorating a specific view with `webmention.middleware.include_webmention_information`
--- webmention/middleware.py
+++ webmention/middleware.py
@@ -1,7 +1,14 @@
 from django.core.urlresolvers import reverse
+from django.utils.decorators import decorator_from_middleware
 
 
 class WebMentionMiddleware(object):
     def process_response(self, request, response):
-        response['Link'] = '<{scheme}://{host}{path}>; rel="webmention"'.format(scheme=request.scheme, host=request.META.get('HTTP_HOST'), path=reverse('webmention:receive'))
+        response['Link'] = '<{scheme}://{host}{path}>; rel="webmention"'.format(
+            scheme=request.scheme,
+            host=request.META.get('HTTP_HOST'),
+            path=reverse('webmention:receive')
+        )
         return response
+
+include_webmention_information = decorator_from_middleware(WebMentionMiddleware)
