15147f924f3c666efbe87e502709a4f217660db7
Dane Hillard <github@danehillard.com>
2015-07-11 13:06:29 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-webmention',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     description='A pluggable implementation of webmention for Django projects.',
     keywords='webmention pingback linkback blogging',
